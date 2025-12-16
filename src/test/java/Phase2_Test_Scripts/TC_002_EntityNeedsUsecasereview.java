0d0720e4b5896bf01baf10c7e3feb2c39a0122f8
PrashanthRao <p.prashanth.rao@gmail.com>
2025-12-16 06:41:31 +0000

    Remove unnecessary blank line in test script

--- src/test/java/Phase2_Test_Scripts/TC_002_EntityNeedsUsecasereview.java
+++ src/test/java/Phase2_Test_Scripts/TC_002_EntityNeedsUsecasereview.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
